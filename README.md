b6415763240a4547f71fbbc1da7832fa6d8e14bf
unknown <CarlosASGI>
2024-05-04 17:32:55 -0300

    Limpiando codigo sin uso y actualizando el archivo README.md e index.js

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-"# webaudio-player" 
+# webaudio-player: Reproductor de audio en la web
+Es un reproductor de audio creado por CarlosASGI, en el que se reproduce el archivo de audio seleccionado previamente para luego escucharlo en el navegador.
+AVISO: Esta pagina es estatica y los archivos no se suben en ningun servidor.
+
--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const inputF=document.getElementById("inputF");
-const title=document.getElementById("title");
-const audio=document.getElementById("audio")
-const btnPlay=document.getElementById("play");
-const btnReload=document.getElementById("reload");
-var inpFile;
+const inputF=document.getElementById("inputF"); //Variable del input del archivo
+const audio=document.getElementById("audio") //Elemento del audio
+const btnPlay=document.getElementById("play"); //Boton de play
+var inpFile; //Archivo seleccionado
 
 inputF.onchange=()=>{
     if(inputF.files.length==1){
-        console.log(inputF.files[0])
-        inpFile=inputF.files[0]
+        //si es un archivo entonces:
+        console.log(inputF.files[0]) //Muestra por la consola la informacion del archivo
+        inpFile=inputF.files[0] //Se declara la variable con el archivo seleecionado
     }
 }
 
 btnPlay.onclick=()=>{
+    //Crea la URL local del audio y lo reproduce
     var uri=window.URL.createObjectURL(inpFile);
     console.log(uri);
     audio.src=uri;
